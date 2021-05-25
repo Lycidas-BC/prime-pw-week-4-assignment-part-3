2b032dde9462aab16ffaa73a4005f5f4dc93d485
Benji Copan <benji1220@gmail.com>
2021-05-25 16:56:42 -0500

    tested code

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -64,12 +64,15 @@ function listItems() {
 
 //Create a function called `removeItem`
 function removeItem( item ) {
+  console.log(`remove item: ${item} from basket: ${basket}`);
   //use array.indexOf to find the index of the first matching item in the basket.
   let itemIndex = basket.indexOf(item);
+  console.log(`first instance of item in basket is at ${itemIndex} (if -1, not in basket)`);
   //make sure item is in array
   if ( itemIndex >= 0 ) {
     //use array.splice to remove the first matching item from the basket.
     basket.splice(itemIndex, 1);
+    console.log(`the basket with the item removed: ${basket}`);
     //return removed item
     return item;
   } else {
@@ -94,6 +97,8 @@ listItems();
 console.log(`Is basket full (should be true): ${isFull()}`);
 console.log(addItem('pineapple'));
 listItems();
+console.log(`Attempting to remove pineapple, which isn't in basket - should be null: ${removeItem('pineapple')}`);
+console.log(`Removing eggs from basket: ${removeItem('eggs')}`);
 empty();
 listItems();
 addItem('apple');
