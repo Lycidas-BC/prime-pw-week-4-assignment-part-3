507340cfce2b1caee88bc29d1f9e6ad1fb424c07
Benji Copan <benji1220@gmail.com>
2021-05-25 16:28:12 -0500

    Create a function called isFull; add console logs for testing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,18 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//GLOBAL VARIABLES (ALPHABETICAL)
+
 //Create a global variable named `basket` and set it to an empty array
 let basket = [];
 //Add a global `const` named `maxItems` and set it to 5.
 const maxItems = 5;
 
+//FUNCTION DEFINITIONS (ALPHABETICAL)
+
 //Create a function called `addItem`
 function addItem( item ){
-  console.log(`addItem - basket is: ${basket}`);
+  console.log(`addItem - basket is: ${basket}, item is ${item}`);
 
   //add the new item to the global array `basket`.
   console.log(`addItem - adding item: ${item}`);
@@ -20,12 +24,26 @@ function addItem( item ){
   return true;
 } //end addItem
 
+//Create a function called `empty`.
 function empty(){
   console.log(`empty - basket is: ${basket}`);
   //reset the `basket` to an empty array
   basket = [];
   console.log(`empty - final basket is: ${basket}`);
 } //end empty
+
+//Create a function called isFull()
+function isFull() {
+  console.log(`isFull - basket is: ${basket}, length: ${basket.length}`);
+  console.log(`isFull - maxItems is: ${maxItems}`);
+  if (basket.length < maxItems){
+    //return `false` if the basket contains *less* than max number of items
+    return false;
+  } else {
+    //return `true` otherwise (equal or more than maxItems)
+    return true;
+  }
+} //end isFull
 
 //Create a function called `listItems`
 function listItems() {
@@ -49,8 +67,10 @@ addItem('butter');
 listItems();
 addItem('flour');
 listItems();
+console.log(`Is basket full (should be false): ${isFull()}`);
 addItem('scotch');
 listItems();
+console.log(`Is basket full (should be true): ${isFull()}`);
 addItem('pineapple');
 empty();
 listItems();
